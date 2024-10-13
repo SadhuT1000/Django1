c6b23fc2ed903c88bc816978a210688092149b69
maksbolomoznov <bolomozhnov@gmail.com>
2024-10-13 12:22:46 +0300

    2233

--- catalog/models.py
+++ catalog/models.py
@@ -1,3 +1,33 @@
+# Create your models here.
 from django.db import models
 
-# Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(max_length=150, verbose_name='Наименование')
+    description = models.TextField(null=True)
+
+    def __str__(self):
+        return f'{self.name}'
+
+    class Meta:
+        verbose_name = 'Категория'
+        verbose_name_plural = 'Категории'
+        ordering = ['name']
+
+
+class Product(models.Model):
+    names = models.CharField(max_length=150, verbose_name='Наименование')
+    description = models.TextField(null=True)
+    image = models.ImageField(upload_to='images/', verbose_name='картинки')
+    category = models.CharField(max_length=150, verbose_name='Категория')
+    price = models.IntegerField()
+    created_at = models.DateField(auto_now_add=True)
+    updated_at = models.DateField(auto_now_add=True)
+
+    def __str__(self):
+        return f'{self.names}'
+
+    class Meta:
+        verbose_name = 'Имя продукта'
+        verbose_name_plural = 'Имена продуктов'
+        ordering = ['names']
